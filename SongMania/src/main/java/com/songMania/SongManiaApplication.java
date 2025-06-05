87c8cd0db9488bd4e80c0f512aade396d97710cf
VishnuVardhan <VishnuVardhan@Rev-PF3C66CF>
2025-06-05 16:49:23 +0530

    Base_project Prototype

---
+++ SongMania/src/main/java/com/songMania/SongManiaApplication.java
@@ -0,0 +1,13 @@
+package com.songMania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongManiaApplication.class, args);
+	}
+
+}
---
+++ SongMania/src/test/java/com/songMania/SongManiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songMania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongManiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
